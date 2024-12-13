a01605829866a544d92ec252cc0d718d64dccf05
NeonFire73 <vladstarovoit421@gmail.com>
2024-12-13 19:45:05 +0200

    added delete and line-throw button

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 $(document).ready(function(){
-    $('#addTask').click(function(){   //функція, яка додає завдання до списку
+    $('#addTask').on("click",function(){   //функція, яка додає завдання до списку при натисканні на плюсик
         let entireText=$('#inputField');
-        $('ul').append('<li class="task">' + entireText.val() + '')
-        entireText.val('')
+        $('ul').append('<li class="task">' +
+            '<button type="button" class="delTask">-</button> ' +
+            entireText.val() +
+            ' ' +
+            '<button type="button" class="done">✓</button>');
+        entireText.val('');
+    });
+    $('.wrapper').on("click",'.delTask',function (){ //Функція, яка видаляє дане завдання
+        let task=$(this).parent();
+        task.remove();
+    });
+    $('.wrapper').on("click", '.done', function () { //Функція, яка робить дане завдання перечеркнутим
+        $(this).parent().css('text-decoration',"line-through");
     });
 });
