edf61b83872b798f1e3e9a55fcd827f35e44d1ba
NeonFire73 <vladstarovoit421@gmail.com>
2024-12-14 17:33:11 +0200

    modified line-through button, modified lists, added some coloros, changed add.png

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 $(document).ready(function(){
     $('#addTask').on("click",function(){   //функція, яка додає завдання до списку при натисканні на плюсик
-        let entireText=$('#inputField');
+        let entireText=$('#inputField'); //зчитує поле вводу
         $('ul').append('<li class="task">' +
-            '<button type="button" class="delTask">-</button> ' +
-            entireText.val() +
+            '<span class="taskText">' +
+            entireText.val() +                          //додає список з текстом із поля вводу,
+            '</span>' +
             ' ' +
-            '<button type="button" class="done">✓</button>');
-        entireText.val('');
+            '<button type="button" class="delTask">-</button> ' + //кнопку видалити
+            '<button type="button" class="done">✓</button>' + //та кнопку перечеркнути текст
+            '</li>' );
+        entireText.val('');//спустошує поле вводу
     });
     $('.wrapper').on("click",'.delTask',function (){ //Функція, яка видаляє дане завдання
         let task=$(this).parent();
         task.remove();
     });
     $('.wrapper').on("click", '.done', function () { //Функція, яка робить дане завдання перечеркнутим
-        $(this).parent().css('text-decoration',"line-through");
+        $('.taskText').toggleClass('lineThrow')
     });
 });
